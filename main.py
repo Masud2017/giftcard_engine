4c37c4771b586e36238e3d45ab7646858bfc472e
Md Masud karim <msmasud578@gmail.com>
2026-08-13 01:46:34 +0200

    Added the comment for the favicon for better understanding

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 
 app.include_router(router)
 
+# setting up the favicon
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse("favicon.ico")
