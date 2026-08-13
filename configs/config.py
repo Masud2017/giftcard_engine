5dafab1588337a839977a56d77bacae3f1df7a84
Md Masud karim <msmasud578@gmail.com>
2026-08-13 02:03:16 +0200

    Added firebase credential to the environment variable also created the firestore instance for later use

--- configs/config.py
+++ configs/config.py
@@ -1,9 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin.firestore import firestore
 import os
 
-python = os.path.join(os.path.abspath(os.curdir),"firebase_config.json")
+firebase_credential_path = os.path.join(os.path.abspath(os.curdir),"firebase_config.json")
 
-cred = credentials.Certificate(python)
-firebase_admin.initialize_app(cred)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = firebase_credential_path
+
+
+cred = credentials.Certificate(firebase_credential_path)
+app = firebase_admin.initialize_app(cred) # keeping the instance for later use
+
+firestore_db = firestore.Client()
 
