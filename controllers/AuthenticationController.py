23afb1da81e3c443900cbd167357f979f7788790
Md Masud karim <msmasud578@gmail.com>
2026-08-23 00:33:24 +0200

    added skeleton for user pass registration controller path

--- controllers/AuthenticationController.py
+++ controllers/AuthenticationController.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
+from models.user import User
+from configs.config import firebase_admin
 
 router = APIRouter()
 
 @router.get("/test")
 async def test_router():
     return "Hello world this is a test router"
+
+
+@router.post({"register_user_pass"})
+def register_user_pass(user: User):
+    pass
