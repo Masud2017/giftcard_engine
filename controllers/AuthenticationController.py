ae59772f156035282234886e158beb15eaa29551
Md Masud karim <msmasud578@gmail.com>
2026-09-13 02:25:54 +0200

    some minor updates

--- controllers/AuthenticationController.py
+++ controllers/AuthenticationController.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter
 from models.user import User
 from configs.config import firebase_admin
 
+from models.model_for_authentication import Token
+from services.authentication_service import AuthenticationService
+
 router = APIRouter()
 
 @router.get("/test")
@@ -12,3 +15,11 @@ async def test_router():
 @router.post({"register_user_pass"})
 def register_user_pass(user: User):
     pass
+
+@router.get("/authenticate")
+def authenticate() -> Token:
+    return AuthenticationService.authenticate()
+
+@router.get("verify")
+def verify()->bool:
+    return False
--- services/authentication_service.py
+++ services/authentication_service.py
@@ -1,6 +1,11 @@
+from models.model_for_authentication import Token
+
 class AuthenticationService:
     def __init__(self):
         pass
     
     def register(self):
         pass
+    @staticmethod
+    def authenticate(self) -> Token:
+        return Token(token_type="asdflkjasdlkfj", token_type = "sdfsdf")
