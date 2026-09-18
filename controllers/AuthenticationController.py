65e27b143a3e1dd3af3be5a20d56c3b27a21d749
Md Masud karim <msmasud578@gmail.com>
2026-09-18 05:44:04 +0200

    Added user authentication route and also updated the user model

--- controllers/AuthenticationController.py
+++ controllers/AuthenticationController.py
@@ -12,9 +12,9 @@ async def test_router():
     return "Hello world this is a test router"
 
 
-@router.post({"register_user_pass"})
-def register_user_pass(user: User):
-    pass
+@router.post("/register_user_pass")
+def register_user_pass(user: User)-> bool:
+    return AuthenticationService.register(email=user.username, password=user.password)
 
 @router.get("/authenticate")
 def authenticate() -> Token:
--- services/authentication_service.py
+++ services/authentication_service.py
@@ -1,11 +1,23 @@
 from models.model_for_authentication import Token
+from firebase_admin import auth
+import logging
 
 class AuthenticationService:
     def __init__(self):
         pass
     
-    def register(self):
-        pass
     @staticmethod
-    def authenticate(self) -> Token:
-        return Token(token_type="asdflkjasdlkfj", token_type = "sdfsdf")
+    def register(email:str,  password:str) -> bool:
+        try:
+            auth.create_user(email = email, password = password)
+            link = auth.generate_email_verification_link(email=email)
+            logging.info(link)
+            print("Printing the stuff: ", link)
+            return True
+        except:
+            return False
+    @staticmethod
+    def authenticate(self,user:str, password:str) -> Token:
+        userid = auth.get_user_by_email(user = user)
+        
+        return Token(access_token="asdflkjasdlkfj", token_type = "sdfsdf")
