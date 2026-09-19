7767888d157e05635311dc450a58925ee3b31c4f
Md Masud karim <msmasud578@gmail.com>
2026-09-19 20:19:28 +0200

    Added firebase firestore repository and also added seller data persisting function in it

---
+++ repositories/firebase_firestore_repository.py
@@ -0,0 +1,16 @@
+from ..configs.config import firestore_db
+from ..models.seller_info import SellerInfo
+import traceback
+
+
+def add_thuis_card_info(seller_info:SellerInfo):
+    try:
+        firestore_db.collection("thuis_card_collection").add({
+                "card_number": seller_info.card_number,
+                "card_pin": seller_info.pin,
+                "first_name": seller_info.first_name,
+                "second_name": seller_info.second_name,
+                "ibn": seller_info.bank_ibn,
+                })
+    except Exception as e:
+        traceback.format_exc(e)
